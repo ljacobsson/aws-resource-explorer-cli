bfab0818b0369a278e62767c5a3776c2afaa5345
Lars Jacobsson <lars.jacobsson@mathem.se>
2022-11-09 11:45:11 +0100

    Update auth-helper.js

--- src/commands/shared/auth-helper.js
+++ src/commands/shared/auth-helper.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 require("@mhlabs/aws-sdk-sso");
 
 function initAuth(cmd) {
-    process.env.AWS_PROFILE = cmd.profile || process.env.AWS_PROFILE || "private";
+    process.env.AWS_PROFILE = cmd.profile || process.env.AWS_PROFILE || "default";
     process.env.AWS_REGION = cmd.region || process.env.AWS_REGION || AWS.config.region
     AWS.config.credentialProvider.providers.unshift(
       new AWS.SingleSignOnCredentials()
